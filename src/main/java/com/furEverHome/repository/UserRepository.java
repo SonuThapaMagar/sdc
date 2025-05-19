a16fb401dfeb351a0e747d4f4e6a7fcac4b2cbcf
sonu <sonumagar787@gmail.com>
2025-05-19 16:18:09 +0545

    Password hashed

--- src/main/java/com/furEverHome/repository/UserRepository.java
+++ src/main/java/com/furEverHome/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.furEverHome.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,5 @@ import com.furEverHome.entity.User;
 
 public interface UserRepository extends JpaRepository<User, UUID>{
 	boolean existsByEmail(String email);
+	Optional<User>findByEmail(String email);
 }
