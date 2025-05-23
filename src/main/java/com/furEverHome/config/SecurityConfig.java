03067021ea1f1d11af30f450b391ae3cab30a42a
sonu <sonumagar787@gmail.com>
2025-05-23 06:14:51 +0545

    Forgot password api

--- src/main/java/com/furEverHome/config/SecurityConfig.java
+++ src/main/java/com/furEverHome/config/SecurityConfig.java
@@ -1,28 +1,38 @@
 package com.furEverHome.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.furEverHome.util.JwtUtil;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-	
-	// The Security Filter Chain is a set of filters that checks every HTTP request.
-	// It handles things like login, logout, authentication, and access control.
-	// Before any request reaches our controller, it goes through these filters
-	// to make sure the user is allowed to access the resource.
+
+	@Autowired
+	private JwtUtil jwtUtil;
+
+	// Filter Chain is a set of filters that checks every HTTP request like login, logout, authentication, and access control.
+	// Before any request reaches our controller, it goes through these filters to make sure the user is allowed to access the resource.
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(auth -> {
-			auth.requestMatchers("/api/**").permitAll() // Allow all /api endpoints for now
-			.anyRequest().authenticated();
-		}).formLogin(form -> form.disable())
+			auth.requestMatchers("/api/**").permitAll()
+				.requestMatchers("/api/admin/**").hasRole("ADMIN")
+				.requestMatchers("/api/superadmin/**").hasRole("SUPERADMIN")
+				.anyRequest().authenticated();
+		})
+		.addFilterBefore(new JwtAuthenticationFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
+		.formLogin(form -> form.disable())
 		.httpBasic(httpBasic -> httpBasic.disable())
 		.csrf(csrf -> csrf.disable());
-												
+
+		System.out.println("Security configuration applied.");
 		return http.build();
 	}
 }
--- src/main/java/com/furEverHome/util/JwtUtil.java
+++ src/main/java/com/furEverHome/util/JwtUtil.java
@@ -1,13 +1,46 @@
 package com.furEverHome.util;
 
+import java.util.Date;
+
 import org.springframework.stereotype.Component;
+
+import com.furEverHome.entity.Role;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
 @Component
 public class JwtUtil {
-	
-	private final String SECRET_KEY ="fureverhome";
+
+	private final String SECRET_KEY = "c3157becaa73426f6da36edea4755c826187bc3c2e6d782111a043cf5471718a";
 	private final long EXPIRATION_TIME = 1000 * 60 * 60 * 365;
-	
-	
+
+	public String generateToken(String email, Role role) {
+		return Jwts.builder().subject(email).claim("role", role.name()).issuedAt(new Date())
+				.expiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+				.signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()), Jwts.SIG.HS512).compact();
+	}
+
+	public String getEmailFromToken(String token) {
+		return Jwts.parser().verifyWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes())).build().parseSignedClaims(token)
+				.getPayload().getSubject();
+	}
+
+	public Role getRoleFromToken(String token) {
+		String role = Jwts.parser().verifyWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes())).build()
+				.parseSignedClaims(token).getPayload().get("role", String.class);
+		return Role.valueOf(role);
+	}
+
+	public boolean validateToken(String token) {
+		try {
+			Jwts.parser().verifyWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes())).build().parseSignedClaims(token);
+			return true;
+		} catch (JwtException | IllegalArgumentException e) {
+			return false;
+		}
+	}
 
 }
