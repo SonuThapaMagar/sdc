ed2bb803cc3c3be6f670abbd6af6792608536637
Sabut Kumar Baidhya <112923215+sabut1214@users.noreply.github.com>
2025-06-22 21:37:27 +0545

    Fixed Merger Conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
-<<<<<<< HEAD
-import "./styles/global.css";
-<<<<<<< HEAD
+import { Routes, Route } from "react-router-dom";
+
 import LandingPage from "./pages/LandingPage";
 import LearnMore from "./pages/users/LearnMore";
 import AboutUs from "./pages/users/AboutUs";
 import ShelterRegistration from "./pages/users/ShelterRegistration";
 import Adoptme from "./pages/users/Adoptme";
 import AdoptionSuccess from "./pages/users/AdoptionSucess";
+import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup";
+import Category from "./pages/users/Category";
 
 export default function App() {
   return (
@@ -21,24 +23,5 @@ export default function App() {
       <Route path="/adoptme/:petId?" element={<Adoptme />} />
       <Route path="/adoption-success/:petId/:applicationId" element={<AdoptionSuccess />} />
     </Routes>
-=======
-import UserRoutes from "./routes/userRoutes";
-
-export default function App() {
-  return (
-    <UserRoutes/>
->>>>>>> 738ef58e456ba53f390dd8a33ee250714938da76
   );
 }
-=======
-import { useRoutes } from 'react-router-dom';
-import superadminRoutes from './routes/superadminRoutes';
-import userRoutes from './routes/userRoutes';
-
-function App() {
-  const routes = useRoutes([...userRoutes, ...superadminRoutes]);
-  return routes;
-}
-
-export default App;
->>>>>>> 3969f1fee02de0f482a859ee1e1072eea98dfe64
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-<<<<<<< HEAD
 import { AuthProvider } from './pages/users/auth-provider';
 import App from './App';
-=======
-import App from './App.jsx';
->>>>>>> 738ef58e456ba53f390dd8a33ee250714938da76
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+  <BrowserRouter>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
 );
